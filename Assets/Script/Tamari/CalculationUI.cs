143f079fe9cf293d6dffcfe1360b727787c9e6b7
MizutamariShutarou <shutaro0415@outlook.jp>
2023-09-27 10:19:17 +0900

    [add] 計算式のUIの表示バグを修正。回答した問題が消える処理を作成。

--- Assets/Script/Tamari/CalculationUI.cs
+++ Assets/Script/Tamari/CalculationUI.cs
@@ -1,5 +1,8 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,23 +17,63 @@ public class CalculationUI : MonoBehaviour
     [SerializeField]
     private OperationObj _operationObj = default;
 
+    [SerializeField]
+    private float _fadeSpeed = 0f;
+
+    [SerializeField]
+    private Ease _fadeEase = Ease.Linear;
+
+    [SerializeField]
+    private float _endPos = 0f;
+
     private List<OperationObj> _operationObjList = new List<OperationObj>();
+
+    // private CancellationToken _ct = new CancellationToken();
 
     void Start()
     {
-        for(int i = 0; i < _generator.FirstGenerateNumber; i++)
+        // _ct = this.GetCancellationTokenOnDestroy();
+        for (int i = 0; i < _generator.FirstGenerateNumber; i++)
         {
-            var obj = GameObject.Instantiate(_operationObj, _canvas.transform);
-            _operationObjList.Add(obj);
-            obj.Initialize(_generator, i);
+            SettingUI(i);
         }
     }
 
-    public void SettingUI(int num)
+    private void SettingUI(int num)
     {
         var obj = GameObject.Instantiate(_operationObj, _canvas.transform);
         _operationObjList.Add(obj);
         obj.Initialize(_generator, num);
-        _operationObjList.Remove(_operationObjList[_generator.CurrentNum - _generator.FirstGenerateNumber]);
     }
+
+    public async UniTask UIAnimation(List<OperationObj> objList)
+    {
+        var image = objList[0].GetComponent<Image>();
+
+        var seq = DOTween.Sequence();
+
+        seq
+            .Join(image.DOFade(0f, _fadeSpeed).SetEase(_fadeEase))
+            .Join(objList[0].transform.DOLocalMoveX(_endPos, _fadeSpeed).SetEase(_fadeEase))
+            .OnComplete(() =>
+            {
+                DestroyUIObj();
+            });
+
+        await UniTask.CompletedTask;
+    }
+
+    private void DestroyUIObj()
+    {
+        Destroy(_operationObjList[0].gameObject);
+        _operationObjList.RemoveAt(0);
+    }
+
+    public async UniTask UpdateUI(int currentNum)
+    {
+        SettingUI(currentNum);
+
+        await UIAnimation(_operationObjList);
+    }
+
 }
