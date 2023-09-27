e0caa21b689805f2df4728cf3bbccd051e74bd8c
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2023-09-27 11:47:40 +0900

    [add]リザルトシーンで終着駅を表示するための機能を追加した。 [update]リザルトシーンを変更した。

---
+++ Assets/Script/Takechi/Result/ResultText.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ResultText : MonoBehaviour
+{
+    [SerializeField] TextType _textType;
+    ResultManager _resultManager;
+    Text _text;
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _resultManager = FindObjectOfType<ResultManager>();
+    }
+
+    void Update()
+    {
+        if(_textType == TextType.MovingDistance)
+            _text.text = $"進んだ距離：{TrainManager._movingDistance}km";
+        if (_textType == TextType.TransitStaion)
+            _text.text = $"終着駅：{_resultManager._transitStaion}";
+    }
+    enum TextType
+    {
+        MovingDistance,
+        TransitStaion,
+        CorrectCount,
+        MaxSpeed,
+    }
+}
